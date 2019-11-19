bf6aaa13a03a21881c968a186f4e97535f4459f8
Daniel Paz <danielpaz@vistomarketing.com.br>
2019-11-19 10:59:02 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,21 +1,23 @@
-function wc_checkout_validation() {
+/*
+		* Function - Disable Checkout for some Countries and States
+	*/
+	function wc_checkout_validation() {
 			$shipping_country     = ! empty( $_POST['shipping_country'] ) ? $_POST['shipping_country'] : $_POST['billing_country'];
 			$shipping_state       = ! empty( $_POST['shipping_state'] ) ? $_POST['shipping_state'] : $_POST['billing_state'];			
 			$countries_obj   = new WC_Countries();
 			$countries   = $countries_obj->__get('countries');
 			$default_country = $countries_obj->get_base_country();
-			$included_states = $countries_obj->get_states( $default_country );
-			// Verification if you are in Base Country
+			$excluded_states = ['RS', 'SC', 'DF'];
+			// Verification if you are in Brazil : More example look here -> wp-content/plugins/woocommerce/i18n/countries.php
 			if ($default_country != $shipping_country) {
 					wc_add_notice( __( 'Desculpem-nos, infelizmente não vendemos para fora do Brasil.', 'odin' ), 'error' );
 			}
-			// States Verification
-			if (!in_array( $shipping_state, $included_states ) ) {
-					// Here you generate State Label for your Alert.
-					$states = WC()->countries->get_states($default_country);
+			// Verification Brazilian States
+			if (in_array($shipping_state, $excluded_states ) ) {
+					// Here you generate State Label for your Alert, if you use a different country. Please, specific below your correct country.
+					$states = WC()->countries->get_states($countries_obj->get_base_country());
 					$state  = $states[$shipping_state];
 					wc_add_notice( sprintf( __( 'Desculpem-nos, infelizmente não realizamos entregas para <strong>%s</strong>.', 'odin' ), $state ), 'error' );
 			}
 	}
-	
 	add_action( 'woocommerce_checkout_process', 'wc_checkout_validation' );
